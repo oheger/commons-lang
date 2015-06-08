0e6b22190ef1868655fec9b3814a2345ad206f76
oheger <oliver.heger@oliver-heger.de>
2015-06-08 22:27:05 +0200

    Initial import of CircuitBreaker classes.

---
+++ src/main/java/org/apache/commons/lang3/concurrent/CircuitBreakingException.java
@@ -0,0 +1,27 @@
+package org.apache.commons.lang3.concurrent;
+
+public class CircuitBreakingException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public CircuitBreakingException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public CircuitBreakingException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public CircuitBreakingException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public CircuitBreakingException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/test/java/org/apache/commons/lang3/concurrent/MemoryCircuitBreakerTest.java
@@ -0,0 +1,26 @@
+package org.apache.commons.lang3.concurrent;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MemoryCircuitBreakerTest {
+
+    @Test
+    public void testThreshold() {
+        long threshold = 10L;
+        MemoryCircuitBreaker circuit = new MemoryCircuitBreaker(threshold);
+        circuit.incrementAndCheckState(9L);
+        circuit.incrementAndCheckState(1L);
+    }
+
+    @Test
+    public void testThresholdCircuitBreakingException() {
+        long threshold = 10L;
+        MemoryCircuitBreaker circuit = new MemoryCircuitBreaker(threshold);
+        assertFalse(circuit.incrementAndCheckState(9L));
+        assertTrue(circuit.incrementAndCheckState(2L));
+    }
+
+}
